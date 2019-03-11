0f704bd01122df81742944f71f5731a937e34da7
Nesar Ramachandra <nayaknesar@gmail.com>
2019-03-11 14:10:52 -0500

    3 param lfi

--- elfi_example.py
+++ elfi_example.py
@@ -2,9 +2,10 @@ import elfi
 import scipy.stats as ss
 import numpy as np
 
-def simulator(mu, sigma, batch_size=1, random_state=None):
-    mu, sigma = np.atleast_1d(mu, sigma)
-    return ss.norm.rvs(mu[:, None], sigma[:, None], size=(batch_size, 30), random_state=random_state)
+def simulator(mu, sigma, alpha, batch_size=1, random_state=None):
+    mu, sigma, alpha = np.atleast_1d(mu, sigma, alpha)
+    return ss.norm.rvs(mu[:, None], (alpha[:, None] + sigma[:, None]), size=(batch_size, 30),
+                       random_state=random_state)
 
 def mean(y):
     return np.mean(y, axis=1)
@@ -15,20 +16,21 @@ def var(y):
 
 mu = elfi.Prior('uniform', -2, 4)
 sigma = elfi.Prior('uniform', 1, 4)
-
+alpha = elfi.Prior('uniform', 2, 4)
 
 # Set the generating parameters that we will try to infer
 mean0 = 1
 std0 = 3
+alpha0 = 3.5
 
 # Generate some data (using a fixed seed here)
 np.random.seed(20170525)
-y0 = simulator(mean0, std0)
+y0 = simulator(mean0, std0, alpha0)
 print(y0)
 
 
 # Add the simulator node and observed data to the model
-sim = elfi.Simulator(simulator, mu, sigma, observed=y0)
+sim = elfi.Simulator(simulator, mu, sigma, alpha, observed=y0)
 
 # Add summary statistics to the model
 S1 = elfi.Summary(mean, sim)
